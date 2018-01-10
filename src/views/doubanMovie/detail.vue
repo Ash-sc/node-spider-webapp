a9c7e57da63907df526994c432dd09f196870408
shenchuang <shenc@getui.com>
2018-01-10 22:49:48 +0800

    add movie detail vue

--- src/views/doubanMovie/detail.vue
+++ src/views/doubanMovie/detail.vue
@@ -2,30 +2,46 @@
   <div class="page-section">
     <div class="movie-preview-section">
       <div class="movie-subject">
-        <!-- <video controls="controls" width="300" height="550">
-          <source src="http://web-site-files.ashshen.cc/blog/spider-webapp/movie-version01.mp4" type="video/mp4" />
-        </video> -->
+        <img :src="movieDetail.imageLink" class="movie-img" v-show="movieDetail.imageLink" >
+        <div class="movie-msg">
+          <h1 class="txt-ellipsis">{{ movieDetail.name }}</h1>
+          <span class="txt-ellipsis">类型：{{ movieDetail.movieType }}</span>
+          <span class="txt-ellipsis">时长：{{ movieDetail.runTime || '-' }}分钟</span>
+          <span class="txt-ellipsis">上映时间：{{ movieDetail.releaseDate || '-' }}</span>
+          <span class="txt-ellipsis">评分：{{ movieDetail.score || '-' }}</span>
+        </div>
       </div>
     </div>
     <div
       class="movie-synopsis"
       :style="{ maxHeight: synopsisType === 'show-more' ? '6.5rem' : '100rem' }"
     >
-      <span class="content">《星球大战：最后的绝地武士》承接前作《星球大战：原力觉醒》的剧情，讲述第一军团全面侵袭之下，蕾伊（黛西·雷德利 Daisy Ridley 饰）、芬恩（约翰·博耶加 John Boyega 饰）、波·达默龙（奥斯卡·伊萨克 Oscar Isaac 饰）三位年轻主角各自的抉 择和冒险故事。前作中觉醒强大原力的蕾伊独自寻访隐居的绝地大师卢克·天行者（马克·哈米尔 Mark Hamill 饰），在后者的指导下接受原力训练。芬恩接受了一项几乎不可能完成的任务，为此他不得不勇闯敌营，面对自己的过去。波·达默龙则要适应从战士向领袖的角色转换，这一过程中他也将接受一些血的教训。</span>
+      <span class="content">{{ movieDetail.synopsis }}</span>
     </div>
     <div
       class="icon-chevron-thin-up"
       :class="synopsisType"
+      v-show="movieDetail.synopsis"
       @click="toggleSynopsis"
     ></div>
+    <spider-loading v-show="isLoading"></spider-loading>
   </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   data() {
     return {
-      synopsisType: 'show-more'
+      synopsisType: 'show-more',
+      isLoading: false
     }
+  },
+
+  computed: {
+    ...mapGetters({
+      movieDetail: 'movieDetail'
+    })
   },
 
   methods: {
@@ -39,8 +55,9 @@ export default {
   created() {
     const { id } = this.$route.params
 
+    this.isLoading = true
     this.$store.dispatch('GET_MOVIE_DETAIL', id).then(data => {
-      console.log(data, 3333)
+      this.isLoading = false
     })
   }
 }
