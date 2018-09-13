413863b1a9217c04a13029080d167aaed3f0e873
shenchuang <shenc@getui.com>
2018-09-13 11:10:56 +0800

    feat(add server): koa server

---
+++ server/scheduleTask/clearMovieCache.js
@@ -0,0 +1,19 @@
+// 定时任务（定时清除无用的电影宣传片资源）
+const schedule = require('node-schedule')
+const path = require('path')
+const fs = require('path')
+
+module.exports = function() {
+
+  schedule.scheduleJob('59 23 * * * 7', function() {
+    let files = []
+    const moviePath = path.join(__dirname, './cache/movieFiles')
+
+    if (fs.existsSync(moviePath)) {
+      files = fs.readdirSync(moviePath)
+      files.forEach(file => {
+        fs.unlinkSync(moviePath + '/' + file)
+      })
+    }
+  })
+}
---
+++ server/spider-server.js
@@ -0,0 +1,38 @@
+const Koa = require('koa')
+const json = require('koa-json')
+const logger = require('koa-logger')
+const koaRouter = require('koa-router')
+const koaBodyparser = require('koa-bodyparser')
+
+const movieList = require('./routes/movieList')
+
+const clearMovieCache = require('./scheduleTask/clearMovieCache')
+
+const fs = require('fs')
+const path = require('path')
+const app = new Koa()
+const router = koaRouter()
+
+app.use(koaBodyparser())
+app.use(json())
+app.use(logger())
+
+app.use(async function(ctx, next) {
+  let start = new Date()
+  await next()
+  let ms = new Date() - start
+  console.log('%s %s - %s', ctx.method, ctx.url, ms)
+})
+
+app.on('error', function (err, ctx) {
+  console.log('server error', err)
+})
+
+router.use('/movieList', movieList.routes())
+
+app.use(router.routes())
+
+clearMovieCache()
+
+
+app.listen(10211)
